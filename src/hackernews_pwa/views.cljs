fe1a27b090f7ebb970a87bed13724bb6df78a958
Faisal Hasnain <faisalhasnain90@gmail.com>
2019-07-30 23:17:10 +0500

    add colour to post item

--- src/hackernews_pwa/views.cljs
+++ src/hackernews_pwa/views.cljs
@@ -5,19 +5,19 @@
 
 (def tabs {:top "Top" :new "New" :ask "Ask" :show "Show" :jobs "Jobs"})
 
-(defn post-item [{:keys [id title url points user time_ago domain comments_count]}]
+(defn post-item [{:keys [id title url domain points user time_ago domain comments_count]}]
   ^{:key id} [:div.box.post-item
               [:div.post-link
                icons/external-link
-               [:a {:href url}
-                [:div.subtitle title]]]
+               [:a {:href url :target "_blank"}
+                [:div [:span.subtitle title] [:span.domain (str " (" domain ")")]]]]
               [:div.post-stats
                [:span icons/thumbs-up points] [:span icons/message-square comments_count] [:span icons/clock time_ago] [:span icons/user user]]])
 
 (defn main-panel []
   (let [loading @(re-frame/subscribe [:get-db :loading])
         tab @(re-frame/subscribe [:get-db :tab])]
-    [:div.pageÍ
+    [:div.page
      [:div.app-title
       [:div.title "Hacker News"]]
      [:div.tabs
